a9aa06be1dcfcf95af05c22e8a35fe765a6a330b
Chum Phalkun <chum_phalkun@yahoo.com>
2013-05-27 09:27:35 +0700

    theara

---
+++ src/Microfis/Simple/Test.php
@@ -0,0 +1,18 @@
+<?php namespace Microfis\Simple;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Theara {
+
+    function __construct() {
+        
+    }
+    public function test()
+    {
+        return "My name is Theara";
+    }
+
+}
+?>
